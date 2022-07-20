5b3c3037ae4e5469a9bfcd14788703e41bb49846
Evanflow89 <ivanbaragone@gmail.com>
2022-07-20 23:05:09 +0200

    Added Model Category

--- database/migrations/2022_07_20_204951_add_column_category_id_posts_table.php
+++ database/migrations/2022_07_20_204951_add_column_category_id_posts_table.php
@@ -14,7 +14,7 @@ class AddColumnCategoryIdPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
+            $table->foreignId('category_id')->nullable()->after('published')->constrained()->onDelete('set null');
         });
     }
 
